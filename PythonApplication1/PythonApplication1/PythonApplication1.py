594534e1dd8f6f1bfcd1e649538c9f0c1b138000
asiyeyildirim <68599181+asiyeyildirim@users.noreply.github.com>
2020-09-20 00:03:54 +0300

    String Metot Örnek

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,17 @@
+"""
 #Girilen Sayının Tek veya Çift Olduğunu Söyleyen Program
 sayi=input('Sayı:')
 if (int(sayi)%2==0):
     print("Sayı Çift")
 else:
     print("Sayı Tek")
+"""
+
+#String Metotları Örnek
+
+site = "http:/www.sadikturan.com"
+course = "Python Kursu: Baştan Sona İleri Seviye Python"
+
+#sitedeki http kısmını silin
+result = site.lstrip('/:htp')
+print(result)
