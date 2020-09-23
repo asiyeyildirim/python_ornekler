30c1c578e74eb3b5aa5a3adcbbbe5b4267fcd502
asiyeyildirim <68599181+asiyeyildirim@users.noreply.github.com>
2020-09-23 19:04:32 +0300

    listeye eleman ekleme

--- PythonApplication2/PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,4 +1,4 @@
-
+"""
 # Python'da Liste İşlemleri
 
 #1  diziyi ve dizi eleman adedini ekrana yazdırma
@@ -6,3 +6,20 @@ il=["istanbul","izmir","antalya","ankara"]
 print(il)
 adet=len(il)
 print(adet)
+"""
+
+#Listeye eleman ekleme işlemleri
+
+#append ile listenin sonuna, aynı anda bir tane olacak şekilde eleman eklenir
+kisi = ["elif"]
+print(kisi)
+kisi.append("buse")
+print(kisi)
+
+#insert ile listenin istediğimiz yerine eleman ekleyebiliriz
+kisi = ["ali"]
+print(kisi)
+kisi.insert(0,"ömer")
+print(kisi)
+kisi.insert(2,"ahmet")
+print(kisi)
