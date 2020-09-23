dd3870605fbea59352c43dd598de9b5d207b781f
asiyeyildirim <68599181+asiyeyildirim@users.noreply.github.com>
2020-09-23 19:28:48 +0300

    dizilerde silme işlemleri

--- PythonApplication2/PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -6,7 +6,7 @@ il=["istanbul","izmir","antalya","ankara"]
 print(il)
 adet=len(il)
 print(adet)
-"""
+
 
 #Listeye eleman ekleme işlemleri
 
@@ -22,4 +22,34 @@ print(kisi)
 kisi.insert(0,"ömer")
 print(kisi)
 kisi.insert(2,"ahmet")
+print(kisi)
+
+#Listeden eleman silme işlemleri
+
+#remove ile listede ilk bulduğu elemanı siler
+kisi=["ali","ahmet","veli"]
+print(kisi)
+kisi.remove("ahmet")
+print(kisi)
+
+#pop ile listede indexe göre silme işlemi yapılır
+
+kisi=["ayse","kubra","meltem"]
+print(kisi)
+kisi.pop(1)
+print(kisi)
+kisi.pop()
+print(kisi)
+
+#del ile silme
+kisi=["burak","ali","hakan"]
+print(kisi)
+del kisi [0:2]
+print(kisi)
+"""
+
+#clear ile silme işleminde tüm dizi silinir
+kisi=["yeliz","ali","burak","ayşe"]
+print(kisi)
+kisi.clear()
 print(kisi)
