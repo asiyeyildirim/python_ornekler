fdb07ca87152a52336e26fccec1e201f2f325dbb
asiyeyildirim <68599181+asiyeyildirim@users.noreply.github.com>
2020-09-30 19:53:32 +0300

    Personel defteri başlangıç

---
+++ Personel_Defteri/Personel_Defteri/Personel_Defteri.py
@@ -0,0 +1,29 @@
+personel=[]
+
+while True:
+    print("::menü::")
+    print("1-Ekleme")
+    print("2-Listeleme")
+    print("3-Arama")
+    print("4-Güncelleme")
+    print("5-Silme")
+    print("6-Çıkış")
+    print("7-Listeyi Gör")
+
+    secim= input("Seçiminiz=")
+
+    if secim=="6":
+        print("Çıkılıyor...")
+        break
+    elif secim=="7":
+        print(personel)
+    elif secim=="1":
+        pass
+    elif secim=="2":
+        pass
+    elif secim=="3":
+        pass
+    elif secim=="4":
+        pass
+    elif secim=="5":
+        pass
