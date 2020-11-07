27bc7b6f025d6d7c635f5b1e8e8b32d331f9d257
asiyeyildirim <68599181+asiyeyildirim@users.noreply.github.com>
2020-11-07 14:45:33 +0300

    atama operatorü örnek

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -29,7 +29,7 @@ print(users)
 
 Liste = [x**3 for x in (2,4,6)]
 print(Liste)
-"""
+
 #Listelerde işlemler
 names=['Ali','Sevim','Ahmet','Eda']
 years=[1998,2005,1988,1975]
@@ -45,3 +45,12 @@ print(names)
 min=min(years) #yıllar arasındaındaki en büyük ve en küçük değeri yazdırma işlemi
 max= max(years)
 print(min,max)
+"""
+#Atama operatörleri Örnek
+x,y,z =2,5,10    #Kullanıcıdan aldığımız iki sayının çarpımı ile x+y+z toplamının farkı
+numbers=1,5,7,10,6
+a=int(input('1.sayı: '))
+b=int(input('2.sayı: '))
+
+result=(a*b)-(x+y+z)
+print(result)
